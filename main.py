864894ebea86f77d48a2058081def564e3d01b0e
harukayamaga <harubik999@gmail.com>
2026-05-17 16:14:04 +0900

    fittbuzz

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from python-kupac!")
+number=input("好きな数字を入れてください:" )
+def fizz_buzz(number):
+    n=int(number)
+    if (n%3==0) and (n%5==0):
+        kan=str("fizz_buzz")
+    elif n%3==0:
+        kan=str("Fizz")
+    elif n%5==0:
+        kan=str("Buzz")
+    else:
+        kan=str(n)
+    return kan
 
+answer=fizz_buzz(number)
+print(answer)
 
-if __name__ == "__main__":
-    main()
+        
